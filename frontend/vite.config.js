0fb68a4f71351c1027cc64d03ee28a6466802596
Rajat083 <155555359+Rajat083@users.noreply.github.com>
2026-01-28 01:35:48 +0530

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:8000',
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
